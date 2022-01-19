da10173f8d14d55231058486292783f8a6c771a9
Alex_ywh <skyywh2014@163.com>
2022-01-19 11:13:15 +0800

    452

---
+++ greedy_algorithm/452.cpp
@@ -0,0 +1,22 @@
+//
+// Created by skyyw on 2022/1/19.
+//
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if(points.empty())
+            return 0;
+        sort(points.begin(), points.end(),[](vector<int>a,vector<int>b) {return a[1]<b[1];});
+        int total=1,prev=points[0][1];
+        for(const vector<int>& ballon :points){//按引用以减少按值复制的时空消耗
+            if(ballon[0]>prev){
+                total++;
+                prev=ballon[1];
+            }
+        }
+        return total;
+    }
+};
